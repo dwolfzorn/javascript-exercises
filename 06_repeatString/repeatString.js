097251d547bf480c78e9ab57242b7753803984f1
David <david.wolfzorn@gmail.com>
2025-06-23 20:35:38 -0400

    Enabled all tests; code passes

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let outputString = '';
+    for (let i = 0; i < num; i++) {
+        outputString += str;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
