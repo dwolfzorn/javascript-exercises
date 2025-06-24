da67152c093e3075b7b10f4945e9bf888c01a5b7
David <david.wolfzorn@gmail.com>
2025-06-23 21:04:48 -0400

    Enabled tests; all tests pass

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    if  (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return "ERROR";
+    }
+    if (firstNum < 0 || lastNum < 0) {
+        return "ERROR";
+    }
+    if (firstNum > lastNum) {
+        const newStart  = firstNum;
+        firstNum = lastNum;
+        lastNum = newStart;
+    }
 
+    let sum = 0;
+    for (let i = firstNum; i <= lastNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
