15455c38114cd89a9ee14ec2d472c1ab07ab135a
David <david.wolfzorn@gmail.com>
2025-06-30 20:53:37 -0400

    Solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const n = Number(num);
 
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
