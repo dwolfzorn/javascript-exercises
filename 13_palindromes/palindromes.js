901794b4da61a4f5d4fa51af9debb15a91dacc71
David <david.wolfzorn@gmail.com>
2025-06-30 20:47:09 -0400

    Solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Remove all whitespace and convert to lowercase
+  const cleaned = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  // Reverse the cleaned string
+  const reversed = cleaned.split('').reverse().join('');
+  // Compare the cleaned string with its reverse
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
